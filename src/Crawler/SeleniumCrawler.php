6f344bba5349f455092558ea9ee8daa94d575cb1
Ivan Li <ivan.li@INVIGOR.local>
2017-07-14 15:03:33 +1000

    added selenium crawler

---
+++ src/Crawler/SeleniumCrawler.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivan.li
+ * Date: 14/07/2017
+ * Time: 2:39 PM
+ */
+
+namespace IvanCLI\Crawler\Repositories;
+
+
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use IvanCLI\Crawler\Contracts\CrawlerContract;
+
+/**
+ * To install selenium, the easiest way will be using Laravel Selenium package "modelizer/selenium": "^1.2"
+ *
+ * Running the following code to install and start firefox and headless driver
+ * # sudo apt-get install firefox xvfb
+ * # Xvfb :10 -ac &
+ * # export DISPLAY=:10
+ * # firefox
+ *
+ *
+ * Running the following artisan command will automatically download and start selenium
+ * # php artisan selenium:start
+ *
+ *
+ *
+ * Class SeleniumCrawler
+ * @package IvanCLI\Crawler\Repositories
+ */
+class SeleniumCrawler implements CrawlerContract
+{
+    const SELENIUM_HOST = 'http://localhost:4444/wd/hub';
+    const SELENIUM_DRVIER = 'firefox';
+
+    protected $driver;
+
+    protected $url;
+    protected $content = null;
+    protected $status = null;
+
+    public function __construct(RemoteWebDriver $remoteWebDriver, DesiredCapabilities $desiredCapabilities)
+    {
+        $this->driver = $remoteWebDriver->create(self::SELENIUM_HOST, $desiredCapabilities->firefox());
+    }
+
+
+    /**
+     * set target URL
+     * @param $url
+     * @return void
+     */
+    public function setURL($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * Load content
+     * @return void
+     */
+    public function fetch()
+    {
+        if (!is_null($this->driver) && !is_null($this->url)) {
+            $this->driver->get($this->url);
+            $this->driver->wait(3);
+            $content = $this->driver->getPageSource();
+            if (!is_null($content) && !empty($content)) {
+                /*
+                 * unfortunately there is no way to get response code from Selenium Webdriver at the moment
+                 * hence, hard-coding 200 to bypass validation
+                 */
+                $this->setStatus(200);
+                $this->setContent($content);
+            }
+        }
+    }
+
+    /**
+     * Get loaded content
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Get request status code
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Update content property
+     * @param $content
+     * @return void
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * Update status property
+     * @param $status
+     * @return void
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+}
